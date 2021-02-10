6b12936e39f30ef55e828cdc94a3dbeb485a926e
Paulo Henrique <phvcandido@gmail.com>
2021-02-10 16:36:32 -0300

    Adicionando controles

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d");
 let box = 32;
 let snake = [];
+
 snake[0] = {
     x: 8 * box,
     y: 8 * box
@@ -21,6 +22,12 @@ function criarCobrinha() {
 }
 
 function iniciarJogo() {
+    if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
+    if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
+    if(snake[0].y > 15 * box && direction == "up") snake[0].y = 0;
+    if(snake[0].y < 0 && direction == "down") snake[0].y = 16 * box;
+
+
     criarBG();
     criarCobrinha();
     let snakeX = snake[0].x;
@@ -41,5 +48,13 @@ function iniciarJogo() {
     snake.unshift(novaCabeca);
 }
 
+document.addEventListener('keydown', update);
+
+function update(event){
+    if(event.keyCode == 37 && direction != "right") direction = "left";
+    if(event.keyCode == 40 && direction != "down") direction = "up";
+    if(event.keyCode == 39 && direction != "left") direction = "right";
+    if(event.keyCode == 38 && direction != "up") direction = "down";
+}
 
 let jogo = setInterval(iniciarJogo, 100);
